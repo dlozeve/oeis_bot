9db00e63c8de338489004dc4a725e18b41e755fb
Dimitri Lozeve <dimitri@lozeve.com>
2026-02-13 08:42:53 +0100

    Use the json feature of ureq to simplify parsing

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,6 @@ use std::fmt;
 #[derive(Debug)]
 pub enum FetchError {
     Http(ureq::Error),
-    Parse(serde_json::Error),
     NotFound(u64),
 }
 
@@ -11,7 +10,6 @@ impl fmt::Display for FetchError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             FetchError::Http(e) => write!(f, "HTTP error: {e}"),
-            FetchError::Parse(e) => write!(f, "JSON parse error: {e}"),
             FetchError::NotFound(id) => write!(f, "sequence A{id:06} not found"),
         }
     }
@@ -21,7 +19,6 @@ impl std::error::Error for FetchError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             FetchError::Http(e) => Some(e),
-            FetchError::Parse(e) => Some(e),
             FetchError::NotFound(_) => None,
         }
     }
@@ -30,11 +27,5 @@ impl std::error::Error for FetchError {
 impl From<ureq::Error> for FetchError {
     fn from(e: ureq::Error) -> Self {
         FetchError::Http(e)
-    }
-}
-
-impl From<serde_json::Error> for FetchError {
-    fn from(e: serde_json::Error) -> Self {
-        FetchError::Parse(e)
     }
 }
